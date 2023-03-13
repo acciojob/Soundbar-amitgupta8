bdf4b1d93f7dd0930de289a8495e1368b2c5fdc2
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-13 16:42:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const buttons = document.getElementById("buttons").children;
+const stopButton = document.getElementsByClassName("stop")[0];
+const sounds = [];
+
+// Push all sounds in the "sounds" array
+for (let i = 1; i <= buttons.length - 1; i++) {
+  sounds.push(new Audio(`sounds/sound${i}.mp3`));
+}
+
+// Add event listeners to the buttons
+for (let i = 0; i < buttons.length - 1; i++) {
+  buttons[i].addEventListener("click", () => {
+    sounds[i].currentTime = 0;
+    sounds[i].play();
+  });
+}
+
+// Add event listener to the stop button
+stopButton.addEventListener("click", () => {
+  sounds.forEach(sound => sound.pause());
+});
+
